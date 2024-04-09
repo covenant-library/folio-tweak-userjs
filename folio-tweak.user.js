05e965f06c90cae24f09504bc4af1f2ef80c99e6
Steve Jamieson <steve.jamieson@covenantseminary.edu>
2024-04-08 21:44:58 -0500

    Update folio-tweak.user.js
    Removed URL checking functions and added more specificity to CSS selectors instead

--- folio-tweak.user.js
+++ folio-tweak.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         FOLIO Tweak
-// @namespace    https://library.covenantseminary.edu/
-// @version      2024-03-28
+// @namespace    http://library.covenantseminary.edu/
+// @version      2024-04-08
 // @description  Make modifications to the FOLIO UI
 // @author       Steve Jamieson
 // @match        https://mobius.folio.ebsco.com/*
@@ -15,10 +15,10 @@
 (function() {
     'use strict';
 
-    if (window.location.href.startsWith("https://mobius.folio.ebsco.com/checkout")) {
-        // Tweaks the checkout UI. Makes the user's name larger and styles the links for open loans, fines/fees, and requests as buttons, which makes them better click targets.
-        GM_addStyle ( `
-#patron-detail div[data-testid="loans"] .kvValue---kU4It a {
+    GM_addStyle ( `
+/* Tweaks the checkout UI. Makes the user's name larger and styles the links for open loans, fines/fees, and requests as buttons, which makes them better click targets. */
+
+#checkout-module-display #patron-detail div[data-testid="loans"] .kvValue---kU4It a {
     border: 1px solid #1960a4;
     padding: 0 15px;
     border-radius: 999px;
@@ -29,33 +29,32 @@
     display: inline-block;
     text-align: center;
 }
-#patron-detail div[data-testid="loans"] .kvValue---kU4It a:hover {
+#checkout-module-display #patron-detail div[data-testid="loans"] .kvValue---kU4It a:hover {
     background-color: rgba(37, 118, 195, .2);
 }
-#patron-detail .active---hPrmG .kvValue---kU4It a {
+#checkout-module-display #patron-detail .active---hPrmG .kvValue---kU4It a {
     color: #1960a4;
 }
-#patron-detail .active---hPrmG .kvValue---kU4It a strong {
+#checkout-module-display #patron-detail .active---hPrmG .kvValue---kU4It a strong {
     font-size: 1.5rem;
 }
-` );
-    } else if (window.location.href.startsWith("https://mobius.folio.ebsco.com/settings/users/perms/")) {
-        // Adds alternate row colors to the list of permissions in a permission set. Particularly handy when removing individual permissions from a set.
-        GM_addStyle ( `
-#assignedPermissions ul.listStyleDefault---VXvtB,
-#permSection ul.listStyleDefault---VXvtB {
+
+
+/* Adds alternate row colors to the list of permissions in a permission set. Particularly handy when removing individual permissions from a set. */
+
+#settings-module-display #assignedPermissions ul.listStyleDefault---VXvtB,
+#form-permission-set #permSection ul.listStyleDefault---VXvtB {
     background-color: transparent;
 }
-#assignedPermissions ul.listStyleDefault---VXvtB li,
-#permSection ul.listStyleDefault---VXvtB li {
+#settings-module-display #assignedPermissions ul.listStyleDefault---VXvtB li,
+#form-permission-set #permSection ul.listStyleDefault---VXvtB li {
     padding-left: 5px;
     padding-right: 5px;
 }
-#assignedPermissions ul.listStyleDefault---VXvtB li:nth-child(odd),
-#permSection ul.listStyleDefault---VXvtB li:nth-child(odd) {
+#settings-module-display #assignedPermissions ul.listStyleDefault---VXvtB li:nth-child(odd),
+#form-permission-set #permSection ul.listStyleDefault---VXvtB li:nth-child(odd) {
     background-color: #00000008;
 }
 ` );
-    }
 
 })();
